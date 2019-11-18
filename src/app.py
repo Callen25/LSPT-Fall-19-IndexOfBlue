188594dfebb0200a8d6e415d80c4ebc7ff08bee4
Matthew Garber <matthewgarber@octant-210.dynamic.rpi.edu>
2019-11-18 11:40:05 -0500

    added spec

--- src/app.py
+++ src/app.py
@@ -1,28 +1,74 @@
 from flask import Flask, request
-from getRequests import exact, union, intersection
-from postRequests import add, delete
+# from getRequests import exact, union, intersection
+# from postRequests import add, delete
 
-@app.route('/exact')
+'''
+@params: http header
+@returns: Json response with list of doc-ids, tf-idf score
+@description: returns data from index that has and exact
+              match with the query
+@throws: returns with response 400 if any error occurs
+'''
+@app.route('/releventDocs')
 def exact_match():
     exact()
     return 'Todo...'
 
-@app.route('/union')
-def union_match():
-    union()
-    return 'Todo...'
+# '''
+# @params: None
+# @returns: Json response with list of doc-ids, tf-idf score
+# @description: returns data from index that has and exact
+#               match with the query
+# @throws: returns with response 400 if any error occurs
+# '''
+# @app.route('/exact')
+# def exact_match():
+#     exact()
+#     return 'Todo...'
 
-@app.route('/intersection')
-def intersect_match():
-    intersection()
-    return 'Todo...'
 
-@app.route('/add')
+# '''
+# @params: None
+# @returns: Json response with list of doc-ids, tf-idf score
+# @description: returns data from index that has any matching
+#               data from a union operation
+# @throws: returns with response 400 if any error occurs
+# '''
+# @app.route('/union')
+# def union_match():
+#     union()
+#     return 'Todo...'
+
+# '''
+# @params: None
+# @returns: Json response with list of doc-ids, tf-idf score
+# @description: returns data from index that has any matching
+#               data from an intersection operation
+# @throws: returns with response 400 if any error occurs
+# '''
+# @app.route('/intersection')
+# def intersect_match():
+#     intersection()
+#     return 'Todo...'
+
+'''
+@params: None
+@returns: 200 ok if the document was added to the index
+@description: adds the new doc-id with transformed text
+@throws: returns with response 400 if any error occurs
+'''
+@app.route('/update')
 def add_document():
-    add(docID)
+    #add(docID)
     return 'Todo...'
 
-@app.route('/remove')
-def remove_document():
-    remove(docID)
-    return 'Todo...'
+# '''
+# @params: None
+# @returns: Json response with list of doc-ids, tf-idf score
+# @description: removes the doc-id and associated transformed text
+# @throws: returns with response 400 if any error occurs
+# '''
+# @app.route('/remove')
+# def remove_document():
+#     remove(docID)
+#     return 'Todo...'
--- src/postRequests.py
+++ src/postRequests.py
@@ -1,8 +1,27 @@
+'''
+@params: parsable json object
+@returns: dictionary with data from json
+@description: creates a object from json which can be used
+              easily in python
+@throws: JsonifyError if any error occurs
+'''
 def parseJSON():
     pass
 
+'''
+@params: args from http
+@returns: 200 ok if added correctly
+@description: gets all of the exact matches from index
+@throws: MatchError if any error occurs
+'''
 def add(docID):
     pass
 
+'''
+@params: args from http
+@returns: set of doc ids
+@description: gets all of the exact matches from index
+@throws: MatchError if any error occurs
+'''
 def remove(docID):
     pass
