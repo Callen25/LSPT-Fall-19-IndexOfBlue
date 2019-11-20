ed75592b630c4844c0ee5a2fc2de55a219143f6f
Nicholas Smith <smithn10@rpi.edu>
2019-11-20 16:52:18 -0500

    Attempt at mock data index made from test examples

--- test/initial_tests.py
+++ test/initial_tests.py
@@ -9,7 +9,17 @@ from flaskr import flaskr
 @pytest.fixture
 def client():
     # make mock index
-    
+    client = dict();
+    client["ocean"] = [(doc1, [0]), (doc2, [1]), (doc3, [1])];
+    client["chore"] = [(doc1, [1])];
+    client["fish"] = [(doc1, [2]), (doc2, [3])];
+    client["sand"] = [(doc1, [3])];
+    client["cup"] = [(doc1, [4])];
+    client["dolphin"] = [(doc2, [0])];
+    client["block"] = [(doc2, [2]), (doc3, [0])];
+    client["star"] = [(doc3, [2])];
+    client["pick"] = [(doc3, [3])];
+    client["spoon"] = [(doc3, [4])];
 
 def test_add_success(client):
     doc1 = "./test_docs/doc1.txt"
